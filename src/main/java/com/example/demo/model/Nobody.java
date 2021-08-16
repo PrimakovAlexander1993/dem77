b23e299fbad855c175a444a1baceb6359447c793
Alexander Primakov <primakovalexander07041993@gmail.com>
2021-08-16 18:57:23 +0300

    MyChanges

---
+++ src/main/java/com/example/demo/model/Nobody.java
@@ -0,0 +1,30 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Nobody {
+    private int id;
+    private String name;
+
+    public Nobody(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
