b9b1698f043bae9ceb37265ce7d6a3828c81a258
Alexander Primakov <primakovalexander07041993@gmail.com>
2021-08-31 19:19:37 +0300

    add customer

--- src/main/java/com/example/demo/model/Customer.java
+++ src/main/java/com/example/demo/model/Customer.java
@@ -4,5 +4,11 @@ import javax.persistence.Entity;
 
 @Entity
 public class Customer {
+    private int x;
+    String name ;
 
+    public Customer(int x, String name) {
+        this.x = x;
+        this.name = name;
+    }
 }
