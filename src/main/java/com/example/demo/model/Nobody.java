4b269fb2bdf09cac74895889d37e2ab3bdaaeae0
Alexander Primakov <primakovalexander07041993@gmail.com>
2021-09-01 13:22:15 +0300

    удалил некоторую информацию из nobody

--- src/main/java/com/example/demo/model/Nobody.java
+++ src/main/java/com/example/demo/model/Nobody.java
@@ -2,29 +2,11 @@ package com.example.demo.model;
 
 import javax.persistence.Entity;
 
-@Entity
+
 public class Nobody {
     private int id;
-    private String name;
 
-    public Nobody(int id, String name) {
+    public Nobody(int id) {
         this.id = id;
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
