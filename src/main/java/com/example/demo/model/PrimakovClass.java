8e8d0b1c09eb9959fbf59b0fa47cd81736e47902
Alexander Primakov <primakovalexander07041993@gmail.com>
2021-09-01 12:53:50 +0300

    добавил класс Primakov

---
+++ src/main/java/com/example/demo/model/PrimakovClass.java
@@ -0,0 +1,10 @@
+package com.example.demo.model;
+
+public class PrimakovClass {
+    int x=5;
+    String name="Я сделла это в ветке Примаков";
+
+    public PrimakovClass(int x) {
+        this.x = x;
+    }
+}
