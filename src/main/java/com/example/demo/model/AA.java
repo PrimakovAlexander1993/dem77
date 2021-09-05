6d33842cfc3566b8fa8308a96352656c997af9cf
Alexander Primakov <primakovalexander07041993@gmail.com>
2021-09-05 18:34:44 +0300

    add smth

---
+++ src/main/java/com/example/demo/model/AA.java
@@ -0,0 +1,6 @@
+package com.example.demo.model;
+
+public class AA {
+    int x;
+    String s;
+}
--- src/main/java/com/example/demo/model/B.java
+++ src/main/java/com/example/demo/model/B.java
@@ -1,5 +1,5 @@
 package com.example.demo.model;
 
 public class B {
-    int f;
+    String s = "Сделал в мастере";
 }
