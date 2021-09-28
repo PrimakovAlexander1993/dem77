6c6de660b225c6f91fd59d7e32a4953a790e3b70
Alexander Primakov <primakovalexander07041993@gmail.com>
2021-09-28 15:10:58 +0300

    add movable

---
+++ src/main/java/com/example/demo/model/A.java
@@ -0,0 +1,4 @@
+package com.example.demo.model;
+
+public class A  implements Swimmable{
+}
---
+++ src/main/java/com/example/demo/model/F.java
@@ -0,0 +1,10 @@
+package com.example.demo.model;
+
+public class F implements Movaable {
+    int g;
+
+    @Override
+    public void move() {
+
+    }
+}
